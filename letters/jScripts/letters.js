fc78c199e20f8228f3daaf364bcb084608b18431
oriallon26 <50099588+oriallon26@users.noreply.github.com>
2021-06-11 15:30:41 +0300

    Add files via upload

--- letters/jScripts/letters.js
+++ letters/jScripts/letters.js
@@ -1,23 +1,27 @@
 ﻿$(document).ready(function () {
 
 
-        //*******************עיצוב דינאמי*********************//
+    //*******************עיצוב דינאמי*********************//
 
     $(".hide").hide();
     $("#downloadLetter").hide();
     $("#letterArea").hide();
+    $("#sideNote").hide();
+    $("#errorNote").css("opacity", 0);
+
+
 
 
     $("#lettetBoss").hover(function () {
         $("#lettetToVol").toggleClass("opacity", "notopacity");
-      //  $("#lettetOrgBoss").toggleClass("opacity", "notopacity");
+        //  $("#lettetOrgBoss").toggleClass("opacity", "notopacity");
         $("#lettetHoliday").toggleClass("opacity", "notopacity");
         $("#lettetvolFam").toggleClass("opacity", "notopacity");
     });
 
     $("#lettetToVol").hover(function () {
         $("#lettetBoss").toggleClass("opacity", "notopacity");
-      //  $("#lettetOrgBoss").toggleClass("opacity", "notopacity");
+        //  $("#lettetOrgBoss").toggleClass("opacity", "notopacity");
         $("#lettetHoliday").toggleClass("opacity", "notopacity");
         $("#lettetvolFam").toggleClass("opacity", "notopacity");
     });
@@ -31,14 +35,14 @@
 
     $("#lettetHoliday").hover(function () {
         $("#lettetToVol").toggleClass("opacity", "notopacity");
-     //   $("#lettetOrgBoss").toggleClass("opacity", "notopacity");
+        //   $("#lettetOrgBoss").toggleClass("opacity", "notopacity");
         $("#lettetBoss").toggleClass("opacity", "notopacity");
         $("#lettetvolFam").toggleClass("opacity", "notopacity");
     });
 
     $("#lettetvolFam").hover(function () {
         $("#lettetToVol").toggleClass("opacity", "notopacity");
-       // $("#lettetOrgBoss").toggleClass("opacity", "notopacity");
+        // $("#lettetOrgBoss").toggleClass("opacity", "notopacity");
         $("#lettetHoliday").toggleClass("opacity", "notopacity");
         $("#lettetBoss").toggleClass("opacity", "notopacity");
     });
@@ -47,21 +51,21 @@
     $("#lettetBoss").click(function () {
         $("#lettetvolFam").css("opacity", 0.5);
         $("#lettetToVol").css("opacity", 0.5);
-      //  $("#lettetOrgBoss").css("opacity", 0.5);
+        //  $("#lettetOrgBoss").css("opacity", 0.5);
         $("#lettetHoliday").css("opacity", 0.5);
         $("#lettetBoss").css("opacity", 1);
     });
     $("#lettetvolFam").click(function () {
         $("#lettetBoss").css("opacity", 0.5);
         $("#lettetToVol").css("opacity", 0.5);
-      //  $("#lettetOrgBoss").css("opacity", 0.5);
+        //  $("#lettetOrgBoss").css("opacity", 0.5);
         $("#lettetHoliday").css("opacity", 0.5);
         $("#lettetvolFam").css("opacity", 1);
     });
     $("#lettetToVol").click(function () {
         $("#lettetBoss").css("opacity", 0.5);
         $("#lettetvolFam").css("opacity", 0.5);
-      //  $("#lettetOrgBoss").css("opacity", 0.5);
+        //  $("#lettetOrgBoss").css("opacity", 0.5);
         $("#lettetHoliday").css("opacity", 0.5);
         $("#lettetToVol").css("opacity", 1);
     });
@@ -75,7 +79,7 @@
     $("#lettetHoliday").click(function () {
         $("#lettetvolFam").css("opacity", 0.5);
         $("#lettetToVol").css("opacity", 0.5);
-      //  $("#lettetOrgBoss").css("opacity", 0.5);
+        //  $("#lettetOrgBoss").css("opacity", 0.5);
         $("#lettetBoss").css("opacity", 0.5);
         $("#lettetHoliday").css("opacity", 1);
     });
@@ -121,7 +125,7 @@
         $(".hide").show();
         $("#familyNameLBL").hide();
         $("#familyName").hide();
-        selectedletter = (this.id); 
+        selectedletter = (this.id);
         console.log("selectedletter- boss" + selectedletter);
     });
 
@@ -133,7 +137,7 @@
         $("#toNameLBL").hide()
         $("#volDurationLBL").hide();
         $("#volDuration").hide();
-   
+
 
     });
 
@@ -173,43 +177,6 @@
         selectedletter = (this.id);
     });
 
-
-    //$("select#letterType").on('change', function () {
-    //    selectedletter = $("#letterType").children("option:selected").val();
-    //    console.log("here")
-    //    //no selection
-    //    if (selectedletter == "0") {
-    //        $(".hide").hide();
-    //        console.log("no selection")
-
-    //    }
-
-    //    //boss
-    //    if (selectedletter == "1") {
-    //        $(".hide").show();
-    //        console.log("boss")
-    //    }
-    //    //thanks to Vol
-    //    if (selectedletter == "2") {
-    //        $(".hide").show();
-    //        $("#toName").hide();
-    //        $("#toNameLBL").hide();
-
-    //    }
-    //    //letter to org
-    //    if (selectedletter == "3") {
-    //        $(".hide").show();
-
-    //    }
-    //    //letters to holidays
-    //    if (selectedletter == "4") {
-    //        $(".hide").show();
-    //        $("#toName").hide();
-    //        $("#toNameLBL").hide();
-
-    //    }
-    //});
-
     //*******************הכנסת טקסט לפי בחירות המשתמש*********************//
     $("#girlIMG").click(function () {
         selectedGender = 1;
@@ -224,15 +191,7 @@
     });
 
     $("#createLetter").click(function () {
-        $("#letterArea").show();
-
-        //selectedletter = $('button').id;
-        //selectedletter = $("#letterType").children("option:selected").val();
-       // selectedGender = $("#gender").children("option:selected").val();
-
-        //selectedGender = $('input[name="gender"]:checked').val();
         console.log("selectedGender clicked to create " + selectedGender)
-
 
         $("#letterArea").html("");
 
@@ -252,38 +211,71 @@
         var BletterThanksToVol = "לכבוד " + voluName + ", \n \nהנדון: בהוקרה והערכה על התנדבותך \n \nשלום " + voluName + ", \n \nברצוני לנצל הזדמנות זו על מנת להביע את תודתי והערכתי העמוקה לפעילותך ההתנדבותית בארגון: על הזמן, המאמץ, העבודה הקשה ועוד- הערכתנו המלאה נתונה לך. \n \nנכונותך לתרום ליכולותיך, ניסיונך וכישוריך חיזקה משמעותית את הארגון שלנו. בתרומתך שימשת דוגמה לכל הסובבים אותך, אודות נתינה, מנהיגות ועשיית טוב. \n \nהחמלה שאתה מפגין כלפי האנשים להם אנחנו מסייעים מעוררת בכולנו השראה רבה. הבחנו כיצד במגוון מצבים אתה תמיד מצליח להתייחס לכולם בטוב לב וכבוד. \nאנחנו יודעים ומבינים שעבור אנשים להם אנחנו מסייעים, החיים רצופים באתגרים ומצבי דחק מילותייך ופעולתיך מסייעות רבות ליכולותם לשמור על כבודם וצמוח מהמשבר. \n \nחשוב לנו לעדכן אותך שהמאמצים שלך והתרומה שלך נראים היטב למרחקים ומייצרים אדוות רבות של השפעה. \n \n תודה רבה על כל מה שעשית ממשיך לעשות. \n \nשלך, \n" + myName + " \nרכזת המתנדבים\n" + orgName + "";
         var BletterToFam = "שלום משפחת " + famName + ", \n \nרצינו להגיד לכם תודה. \n \nתודה שחלקתם את הזמן של " + voluName + " איתנו, שאפשרתם לו להשקיע מאמצים רבים לטובת " + volGoal + ". \n \nאנו יודעים שגם אתם משלמים על כך מחיר וחשוב לנו לספר לכם שיש למחיר הזה משמעות רבה מאוד עבורנו. התרומה של " + voluName + " לארגוננו היא כה רבה. \nהניסיון, הידע והיכולות שהוא תורמת לארגון מאפשרים לנו לעשות כל כך הרבה יותר על מנת להשיג את מטרותינו. \n \nתודה על התמיכה מאחורי הקלעים, על העידוד מהיציע, על ההבנה ובעיקר תודה על " + voluName + " הנפלא. \n \nשלכם, \n" + myName + " \n" + orgName + ""
 
-       //var BletterToOrg = "מכתב למנהל בארגון גבר";
+        //var BletterToOrg = "מכתב למנהל בארגון גבר";
 
 
         var GletterToBoss = "שלום " + letterFor + ",\n \nרצינו לספר לך על " + voluName + ". אנחנו יודעים שאת/ה מכיר/ה אותה היטב אבל חשבנו שזו יכולה להיות הזדמנות נפלאה, להאיר אותה באור נוסף ולהדגיש צדדים נוספים שלה- דרך תרומתה בארגון שלנו. \n" + voluName + " הצטרפה לפני " + volDuration + ", במסגרת השותפות בין הארגון שלנו לבין חברתכם. " + voluName + " מפגינה מחויבות והתלהבות הולכת וגדלה  בתפקידה ותורמת באופן משמעותי להשגת מטרות הארגון.\n \n" + voluName + " מביאה לארגון שלנו ידע ייחודי, נסיון יוצא דופן שבוודאי צברה בחברתכם, חמלה והקשבה, רצינות ומחויבות. \nהנתינה והאכפתיות שלה הן הרבה מעבר למצופה ויש לה תרומה אדירה להצלחתנו. \n \nאנו רוצים להודות לך על השותפות והנכונות לאפשר לה, כעובדת, לתרום לקהילה, לקחת חלק בפעילות שלנו ולסייע לנו לקדם את " + volGoal + ". \nאנו מאמינים ומקווים שכשם שהיא תורמת לנו היא גם נתרמת מהפעילות ומחזירה את שלמדה אל מסדרונות החברה- במוטיבציה, בהערכה ובתחושת השייכות. \n \nבברכה, \n" + myName + " \nרכז ההתנדבות \n" + orgName + "";
-        var GletterToHoliday = "" + voluName + " היקרה, \n \nחגים מספקים לנו הזדמנות נהדרת לנוח, לטעון מצברים, לעצור, להעריך את מה שיש- ולהודות. \n \nאז רצינו להגיד לך תודה, " + voluName + ", על התרומה המשמעותית שלך לפעילות הארגון. \nאת מעוררת בנו השראה, דוחפת אותנו להצליח, מחייבת אותנו להעמיק ולחשוב, מעודדת אותנו לצחוק קצת יותר בקול ולחייך בכל הזדמנות שיש לנו. את נהדרת ואיזה כיף שאת איתנו \n \nאנו מקווים שיהיה לך חג שמח מאוד, מלא באושר, שמחה ושלום. \n \nמכולנו ב" + orgName+", חג שמח!!";
+        var GletterToHoliday = "" + voluName + " היקרה, \n \nחגים מספקים לנו הזדמנות נהדרת לנוח, לטעון מצברים, לעצור, להעריך את מה שיש- ולהודות. \n \nאז רצינו להגיד לך תודה, " + voluName + ", על התרומה המשמעותית שלך לפעילות הארגון. \nאת מעוררת בנו השראה, דוחפת אותנו להצליח, מחייבת אותנו להעמיק ולחשוב, מעודדת אותנו לצחוק קצת יותר בקול ולחייך בכל הזדמנות שיש לנו. את נהדרת ואיזה כיף שאת איתנו \n \nאנו מקווים שיהיה לך חג שמח מאוד, מלא באושר, שמחה ושלום. \n \nמכולנו ב" + orgName + ", חג שמח!!";
 
-        var GletterThanksToVol = "לכבוד " + voluName + ", \n \nהנדון: בהוקרה והערכה על התנדבותך \n \nשלום " +voluName + ", \n \nברצוני לנצל הזדמנות זו על מנת להביע את תודתי והערכתי העמוקה לפעילותך ההתנדבותית בארגון: על הזמן, המאמץ, העבודה הקשה ועוד- הערכתנו המלאה נתונה לך. \n \nנכונותך לתרום ליכולותיך, ניסיונך וכישוריך חיזקה משמעותית את הארגון שלנו. בתרומתך שימשת דוגמה לכל הסובבים אותך, אודות נתינה, מנהיגות ועשיית טוב. \n \nהחמלה שאת מפגינה כלפי האנשים להם אנחנו מסייעים מעוררת בכולנו השראה רבה. הבחנו כיצד במגוון מצבים את תמיד מצליחה להתייחס לכולם בטוב לב וכבוד. \nאנחנו יודעים ומבינים שעבור אנשים להם אנחנו מסייעים, החיים רצופים באתגרים ומצבי דחק מילותייך ופעולתיך מסייעות רבות ליכולותם לשמור על כבודם וצמוח מהמשבר. \n \nחשוב לנו לעדכן אותך שהמאמצים שלך והתרומה שלך נראים היטב למרחקים ומייצרים אדוות רבות של השפעה. \n \n תודה רבה על כל מה שעשית וממשיכה לעשות. \n \nשלך, \n" + myName + " \nרכזת המתנדבים\n" + orgName+"";
+        var GletterThanksToVol = "לכבוד " + voluName + ", \n \nהנדון: בהוקרה והערכה על התנדבותך \n \nשלום " + voluName + ", \n \nברצוני לנצל הזדמנות זו על מנת להביע את תודתי והערכתי העמוקה לפעילותך ההתנדבותית בארגון: על הזמן, המאמץ, העבודה הקשה ועוד- הערכתנו המלאה נתונה לך. \n \nנכונותך לתרום ליכולותיך, ניסיונך וכישוריך חיזקה משמעותית את הארגון שלנו. בתרומתך שימשת דוגמה לכל הסובבים אותך, אודות נתינה, מנהיגות ועשיית טוב. \n \nהחמלה שאת מפגינה כלפי האנשים להם אנחנו מסייעים מעוררת בכולנו השראה רבה. הבחנו כיצד במגוון מצבים את תמיד מצליחה להתייחס לכולם בטוב לב וכבוד. \nאנחנו יודעים ומבינים שעבור אנשים להם אנחנו מסייעים, החיים רצופים באתגרים ומצבי דחק מילותייך ופעולתיך מסייעות רבות ליכולותם לשמור על כבודם וצמוח מהמשבר. \n \nחשוב לנו לעדכן אותך שהמאמצים שלך והתרומה שלך נראים היטב למרחקים ומייצרים אדוות רבות של השפעה. \n \n תודה רבה על כל מה שעשית וממשיכה לעשות. \n \nשלך, \n" + myName + " \nרכזת המתנדבים\n" + orgName + "";
         //   var GletterToOrg = "מכתב למנהל בארגון אישה";
-        var GletterToFam = "שלום משפחת " + famName + ", \n \nרצינו להגיד לכם תודה. \n \nתודה שחלקתם את הזמן של " + voluName + " איתנו, שאפשרתם לה להשקיע מאמצים רבים לטובת " + volGoal + ". \n \nאנו יודעים שגם אתם משלמים על כך מחיר וחשוב לנו לספר לכם שיש למחיר הזה משמעות רבה מאוד עבורנו. התרומה של " + voluName + " לארגוננו היא כה רבה. \nהניסיון, הידע והיכולות שהיא תורמת לארגון מאפשרים לנו לעשות כל כך הרבה יותר על מנת להשיג את מטרותינו. \n \nתודה על התמיכה מאחורי הקלעים, על העידוד מהיציע, על ההבנה ובעיקר תודה על " + voluName + " הנפלאה. \n \nשלכם, \n" + myName + " \n" + orgName+""
+        var GletterToFam = "שלום משפחת " + famName + ", \n \nרצינו להגיד לכם תודה. \n \nתודה שחלקתם את הזמן של " + voluName + " איתנו, שאפשרתם לה להשקיע מאמצים רבים לטובת " + volGoal + ". \n \nאנו יודעים שגם אתם משלמים על כך מחיר וחשוב לנו לספר לכם שיש למחיר הזה משמעות רבה מאוד עבורנו. התרומה של " + voluName + " לארגוננו היא כה רבה. \nהניסיון, הידע והיכולות שהיא תורמת לארגון מאפשרים לנו לעשות כל כך הרבה יותר על מנת להשיג את מטרותינו. \n \nתודה על התמיכה מאחורי הקלעים, על העידוד מהיציע, על ההבנה ובעיקר תודה על " + voluName + " הנפלאה. \n \nשלכם, \n" + myName + " \n" + orgName + ""
 
 
 
         console.log(selectedletter + "selectedletter");
+        console.log($("myName").val() + " myName");
+
 
         if (selectedletter == "lettetBoss") {
-            if (selectedGender == "1") {
-                $("#letterArea").html(GletterToBoss);
-                console.log(letterFor + "ritush");
+            if (voluName != "" && myName != "" && letterFor != "" && volGoal != "" && volDuration != "" && orgName != "") {
+                if (selectedGender == "1" || selectedGender == "2") {
 
+                    if (selectedGender == "1") {
+
+                        $("#letterArea").html(GletterToBoss);
+                        console.log(letterFor + "ritush");
+
+                    }
+                    if (selectedGender == "2") {
+
+                        $("#letterArea").html(BletterToBoss);
+                        console.log(letterFor);
+                    }
+
+                    $("#letterArea").show();
+                    $("#sideNote").show();
+                    $("#downloadLetter").show();
+                    $("#errorNote").css("opacity", 0);
+                }
             }
-            if (selectedGender == "2") {
-                $("#letterArea").html(BletterToBoss);
-                console.log(letterFor);
+            else {
+
+                $("#errorNote").css("opacity", 1);
             }
         }
 
         if (selectedletter == "lettetToVol") {
-            if (selectedGender == "1") {
-                $("#letterArea").html(GletterThanksToVol);
+            if (voluName != "" && myName != "" && orgName != "") {
+                if (selectedGender == "1" || selectedGender == "2") {
+
+                }
+                if (selectedGender == "1") {
+                    $("#letterArea").html(GletterThanksToVol);
+                }
+                if (selectedGender == "2") {
+                    $("#letterArea").html(BletterThanksToVol);
+                }
+
+                $("#letterArea").show();
+                $("#sideNote").show();
+                $("#downloadLetter").show();
+                $("#errorNote").css("opacity", 0);
+
             }
-            if (selectedGender == "2") {
-                $("#letterArea").html(BletterThanksToVol);
+            else {
+
+                $("#errorNote").css("opacity", 1);
             }
         }
 
@@ -297,29 +289,51 @@
         //}
 
         if (selectedletter == "lettetHoliday") {
-            if (selectedGender == "1") {
-                $("#letterArea").html(GletterToHoliday);
+            if (voluName != "" && orgName != "") {
+                if (selectedGender == "1" || selectedGender == "2") {
+                }
+                if (selectedGender == "1") {
+                    $("#letterArea").html(GletterToHoliday);
+                }
+                if (selectedGender == "2") {
+                    $("#letterArea").html(BletterToHoliday);
+                }
+
+                $("#letterArea").show();
+                $("#sideNote").show();
+                $("#downloadLetter").show();
+                $("#errorNote").css("opacity", 0);
             }
-            if (selectedGender == "2") {
-                $("#letterArea").html(BletterToHoliday);
+            else {
+                $("#errorNote").css("opacity", 1);
+
             }
+
         }
 
         if (selectedletter == "lettetvolFam") {
-            if (selectedGender == "1") {
-                $("#letterArea").html(GletterToFam);
-                console.log("בחורה מכתב למשפחה")
-            }
-            if (selectedGender == "2") {
-                $("#letterArea").html(BletterToFam);
+            if (voluName != "" && myName != "" && famName != "" && volGoal != "" && orgName != "") {
+                if (selectedGender == "1" || selectedGender == "2") {
+                    if (selectedGender == "1") {
+                        $("#letterArea").html(GletterToFam);
+                        console.log("בחורה מכתב למשפחה")
+                    }
+                    if (selectedGender == "2") {
+                        $("#letterArea").html(BletterToFam);
+                    }
+                    $("#letterArea").show();
+                    $("#sideNote").show();
+                    $("#downloadLetter").show();
+                    $("#errorNote").css("opacity", 0);
+                }
+                else {
+                    $("#errorNote").css("opacity", 1);
+
+                }
             }
         }
-        $("#downloadLetter").show();
-
-
     });
 
-  
 });
 
 
@@ -328,7 +342,7 @@
 //*******************פונקציה להורדת המכתב*********************//
 function saveTextAsFile(textToWrite, fileNameToSaveAs) {
 
-   
+
 
     var textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
     var downloadLink = document.createElement("a");
@@ -350,3 +364,5 @@ function saveTextAsFile(textToWrite, fileNameToSaveAs) {
 
     downloadLink.click();
 }
+
+
